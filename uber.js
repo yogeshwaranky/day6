2bd4349b4202a3c7d54acf1530ece866eaa263f0
yogeshwaranky <yogeshwaranky2103@email.com>
2024-03-27 16:48:50 +0530

    hi

--- uber.js
+++ uber.js
@@ -1,7 +1,7 @@
 let baseFee = .88
 let cities = ["mayiladuthurai", "ooty", "kodaikanal" ]
 let uberRates = [15, 20, 25]
-let customerName = "Sanofer" 
+let customerName = "yogi" 
 let customerCity = "mayiladuthurai"
 console.log("Hello", customerName+ ", welcome to the Uber Rate Program")
 function getRate(customerCity) {
